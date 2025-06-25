16f95d11b180a96070ad7511bdd373fd31ffa333
AlonePyWarrior <ali.esmaeilzadeh.py.com>
2025-06-26 00:08:00 +0330

    deployed in bad internet!

--- config/settings.py
+++ config/settings.py
@@ -123,6 +123,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
+# LANGUAGE_CODE = 'fa-ir'
+LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Asia/Tehran'
 USE_I18N = True
 USE_L10N = True
