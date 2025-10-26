00835686bf442adb8fbfd62eb2b184d5a69e0a12
rockstarsolider <pouriaghiasi3@gmail.com>
2025-10-26 21:17:04 +0330

    step list & mentor feedback templates

--- courses/urls.py
+++ courses/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.LearnerDashboardView.as_view(), name='learner-dashboard'),
+    path('step_list/', views.StepListView.as_view(), name='step-list'),
+    path('mentor_feedback_list/', views.MnetorFeedbackListView.as_view(), name='mentor-feedback-list'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views.generic import View, ListView
 
 # Create your views here.
-class LearnerDashboardView(generic.TemplateView):
-    template_name = 'courses/learner_dash.html'
+class LearnerDashboardView(View):
+    def get(self, request):
+        return render(request, 'courses/learner_dash.html')
+    
+class StepListView(ListView):
+    def get(self, request):
+        return render(request, 'courses/step_list.html')
+    
+class MnetorFeedbackListView(ListView):
+    def get(self, request):
+        return render(request, 'courses/mentor_feedback_list.html')
